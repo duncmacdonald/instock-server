15e4f5f85ba9dc7c5e3888efcde5bcecf07fa2bd
NoahLaroi <noahll597@gmail.com>
2022-04-05 22:18:33 -0400

    backend to match SPG3-20

---
+++ instock-server-main/routes/warehouse.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+
+require('dotenv').config();
+const {PORT, BACKEND_URL} = process.env;
+
+
+// Read data file
+function readVideos() {
+    const videoData = fs.readFileSync("./data/warehouse.json");
+    const parsedData = JSON.parse(videoData);
+    return parsedData;
+}
+
+// Write data file
+function writeVideos(data){
+    fs.writeFileSync("./data/warehouses.json", JSON.stringify(data));
+}
+
+
+// Get condensed list of video information
+router.route("/")
+    .get((req, res) => {   
+        console.log("warehouse posting endpoint");
+        res.status(200).send("warehouse posting endpoint");
+    })
+    .post((req, res) => {
+        console.log("warehouse posting endpoint");
+        res.status(200).send("warehouse posting endpoint");;
+    });
+
+
+
+module.exports = router;
